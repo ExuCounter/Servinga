65003913013fb0e37900133fcb28cb15ed4f6f68
Exucounter <exucounter777@gmail.com>
2020-04-13 22:20:49 +0300

    Media queries

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,8 @@
 let burgerMenu = document.querySelector('.burger-menu'); // BURGER
-let topBarNavCol = document.querySelector('.top-bar__row-col__nav'); // CONTAINER
-let menuLinks = document.querySelectorAll('.top-bar__row-col__nav a')
+let topBarNavCol = document.querySelector('.top-bar__row__nav-col__list'); // CONTAINER
+let menuLinks = document.querySelectorAll('.top-bar__row-col__nav a');
+let closeMenuBtn = document.querySelector('.close-btn');
+
 burgerMenu.addEventListener('click', ()=>{
     if(!(topBarNavCol.classList.contains('active'))){  
         burgerMenu.classList.add('active');
@@ -12,9 +14,7 @@ burgerMenu.addEventListener('click', ()=>{
     }
 })
 
-menuLinks.forEach((item)=>{
-    item.onclick = ()=>{
-        burgerMenu.classList.remove('active');
-        topBarNavCol.classList.remove('active');
-    }
-})
+closeMenuBtn.addEventListener('click', ()=>{
+    burgerMenu.classList.remove('active');
+    topBarNavCol.classList.remove('active');
+});
